df08309d2c1748a8a3fd2da75e6c6af628d9c2bf
ARYAN KOODI <127104749+aryank16@users.noreply.github.com>
2026-03-19 23:59:35 +0100

    routing

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+import Body from "./Body"
+import Profile from "./Profile"
+import Login from "./Login"
+
+
+function App() {
+  
+
+  return (
+    <>
+
+    <BrowserRouter basename="/">
+    <Routes>
+     
+       <Route path="/" element={<Body/>}>
+       <Route path="/login" element={<Login/>}/>
+       <Route path="/profile" element={<Profile/>}></Route>
+       
+       </Route>
+
+     
+    </Routes>
+    
+
+    </BrowserRouter>
+    
+    </>
+  )
+}
+
+export default App
