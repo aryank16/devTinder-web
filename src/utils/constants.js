46b5c02635bcffa8b08dbdcf463a94268ba34793
ARYAN KOODI <127104749+aryank16@users.noreply.github.com>
2026-03-30 23:57:57 +0200

    made base url dynamic

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,9 @@
+ 
+ //production
+// export const BASE_URL = '/api';
 
-export const BASE_URL = '/api';
+
+// development
+export const BASE_URL =
+
+location.hostname === 'localhost' ? 'http://localhost:3000' : '/api';
